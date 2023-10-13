b27cc7c0de3c4d849b6616b0cf70ced51802b824
skycoco <skycoco1994@gmail.com>
2023-10-13 13:29:31 +1300

    update algolia

--- .vitepress/setting.ts
+++ .vitepress/setting.ts
@@ -27,10 +27,10 @@ export function createSocialLinks(): SocialLink[] {
 
 export function createAlgolia() {
   return {
-    apiKey: "b660c80f7535631292cb660a98e85d97",
-    indexName: "codespacex-",
+    apiKey: "45b6bb84224e68593eeda867627c0925",
+    indexName: "codespacex",
     // 如果 Algolia 没有为你提供 `appId` ，使用 `BH4D9OD16A` 或者移除该配置项
-    appId: "260HZ0WV1R",
+    appId: "P808QIPILT",
     placeholder: "Keywords for search docs",
     translations: {
       button: {
